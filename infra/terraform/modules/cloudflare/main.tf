c0ab8df84ba66428692410d904325d2751d2be02
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-08-22 13:20:19 -0300

    enable the cloudflare provider

--- infra/terraform/modules/cloudflare/main.tf
+++ infra/terraform/modules/cloudflare/main.tf
@@ -1,13 +1,13 @@
-locals {
-  cloudflare_account   = var.cloudflare_account
-  cloudflare_dns_token = var.cloudflare_dns_token
-}
+# locals {
+#   cloudflare_account   = var.cloudflare_account
+#   cloudflare_dns_token = var.cloudflare_dns_token
+# }
 
 data "cloudflare_zone" "domain" {
   name = var.cloudflare_domain
 }
 
-resource "cloudflare_record" "www" {
+resource "cloudflare_record" "record" {
   zone_id = data.cloudflare_zone.domain.id
   name    = "gitops"
   value   = var.cloudflare_dns_address
